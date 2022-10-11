d3b471300cb6a07c0e938aee1b7b1854c36ca444
Jo Thomas <jothomas1996@gmail.com>
2022-10-11 22:42:48 +0530

    completed 1

---
+++ basics_1/README.md
@@ -0,0 +1,7 @@
+# Code Challenge 1
+
+1. Make a list of your favourite Movies, the list should have minimum 8 elements.
+2. Print a specified list after removing the 5th element.
+3. Insert your favourite movie director name at the 4th index position of the list and print out the list elements.
+4. List out the 4th element in the list.
+5. Add additional item to the current list and display the list.
---
+++ basics_1/main.py
@@ -0,0 +1,23 @@
+# Make a list of your favourite Movies, the list should have minimum 8 elements.
+
+movies = ["Ready Player One", "Jurassic World Dominion", "Transformers", "Terminator: Dark Fate", "Sonic the Hedgehog 2", "The Tomorrow War", "Edge of Tomorrow", "E.T. the Extra-Terrestrial"]
+
+# Print a specified list after removing the 5th element.
+
+del movies[4]
+print(movies)
+
+# Insert your favourite movie director name at the 4th index position of the list and print out the list elements.
+
+movies.insert(4, "Michael Bay")
+print(movies)
+
+# List out the 4th element in the list.
+
+print(movies[4])
+
+# Add additional item to the current list and display the list.
+
+movies.append("Independence Day")
+movies.append("Battleship")
+print(movies)
