04a068b1c0c18d788e14dffa29ac278e691e1f01
Jo Thomas <jothomas1996@gmail.com>
2022-10-11 23:23:28 +0530

    completed 3

---
+++ basics_3/README.md
@@ -0,0 +1,9 @@
+# Code Challenge 3
+
+Create a function , and calculated using the formula:
+
+Final Price(FP) = (Product Price of of X )/(Product Price of Y)^2.
+
+Write python code which can accept the price X and price of Y of a Product and calculate the FP.
+
+Note: Make sure to use a function which accepts the X and Y values and returns the FP value.
---
+++ basics_3/main.py
@@ -0,0 +1,14 @@
+# Create a function , and calculated using the formula:
+
+# Final Price(FP) = (Product Price of of X )/(Product Price of Y)^2.
+
+# Write python code which can accept the price X and price of Y of a Product and calculate the FP.
+
+# Note: Make sure to use a function which accepts the X and Y values and returns the FP value.
+
+def final_price(x, y):
+	return (x/y)**2
+
+x = float(input("Enter product price of x: "))
+y = float(input("Enter product price of y: "))
+print("Final Price:", final_price(x, y))
