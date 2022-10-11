f9e287d4d955324035d61378c5d2895b05523265
Jo Thomas <jothomas1996@gmail.com>
2022-10-12 00:07:16 +0530

    completed 5

---
+++ basics_5/README.md
@@ -0,0 +1,5 @@
+# Code Challenge 5
+
+1. Write Python code to open a file named Fileprogram.txt and write some random data into it.
+2. Open the file, read the contents and display them as output.
+3. Write python code to add additional text to the existing file on a new line without deleting the previous contents.
---
+++ basics_5/main.py
@@ -0,0 +1,20 @@
+# Write Python code to open a file named Fileprogram.txt and write some random data into it.
+
+file = open("Fileprogram.txt", "w")
+content = "This is test file"
+file.write(content)
+file.close()
+
+# Open the file, read the contents and display them as output.
+
+file = open("Fileprogram.txt", "r")
+content = file.read()
+print(content)
+file.close()
+
+# Write python code to add additional text to the existing file on a new line without deleting the previous contents.
+
+file = open("Fileprogram.txt", "a")
+content = "This is a new line"
+file.write(content)
+file.close()
