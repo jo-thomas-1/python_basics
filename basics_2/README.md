9c30fa286c7cf706366fbd73a672b53e97d5dfcb
Jo Thomas <jothomas1996@gmail.com>
2022-10-11 22:54:47 +0530

    completed 2

---
+++ basics_2/README.md
@@ -0,0 +1,16 @@
+# Code Challenge 2
+
+1. Using a for-loop,
+
+- calculates the addition of each number
+
+Eg:
+INPUT
+X= [10,20,30,40]
+OUTPUT
+20
+40
+60
+80
+
+- Print “I Love My India" 7 times
---
+++ basics_2/main.py
@@ -0,0 +1,10 @@
+# calculates the addition of each number
+
+data = [10, 20, 30, 40]
+for value in data:
+	print(value + value)
+
+# Print “I Love My India" 7 times
+
+for i in range(7):
+	print("I Love My India")
